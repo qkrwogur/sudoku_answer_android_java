bc64dec6875a8c6a5e866b737c96bb3a02509fe1
qkrwogur <qkrwogur1001@nate.com>
2021-08-22 15:30:03 +0900

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,332 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    int before = 100;//이전버튼을 누룬 것의 저장징소
+    int touchX,touchY;
+    int flag=0;
+
+    int checkInt=0;
+    int[][] grid= new int[][]
+            {
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0},
+                    {0, 0, 0, 0, 0, 0, 0, 0, 0}
+            };
+    Button button[] = new Button[81];
+    Integer[] btnID={
+            R.id.a_1, R.id.b_1, R.id.c_1, R.id.d_1, R.id.e_1,
+            R.id.f_1, R.id.h_1, R.id.i_1, R.id.j_1,
+
+            R.id.a_2, R.id.b_2, R.id.c_2, R.id.d_2, R.id.e_2,
+            R.id.f_2, R.id.h_2, R.id.i_2, R.id.j_2,
+
+            R.id.a_3, R.id.b_3, R.id.c_3, R.id.d_3, R.id.e_3,
+            R.id.f_3, R.id.h_3, R.id.i_3, R.id.j_3,
+
+            R.id.a_4, R.id.b_4, R.id.c_4, R.id.d_4, R.id.e_4,
+            R.id.f_4, R.id.h_4, R.id.i_4, R.id.j_4,
+
+            R.id.a_5, R.id.b_5, R.id.c_5, R.id.d_5, R.id.e_5,
+            R.id.f_5, R.id.h_5, R.id.i_5, R.id.j_5,
+
+            R.id.a_6, R.id.b_6, R.id.c_6, R.id.d_6, R.id.e_6,
+            R.id.f_6, R.id.h_6, R.id.i_6, R.id.j_6,
+
+            R.id.a_7, R.id.b_7, R.id.c_7, R.id.d_7, R.id.e_7,
+            R.id.f_7, R.id.h_7, R.id.i_7, R.id.j_7,
+
+            R.id.a_8, R.id.b_8, R.id.c_8, R.id.d_8, R.id.e_8,
+            R.id.f_8, R.id.h_8, R.id.i_8, R.id.j_8,
+
+            R.id.a_9, R.id.b_9, R.id.c_9, R.id.d_9, R.id.e_9,
+            R.id.f_9, R.id.h_9, R.id.i_9, R.id.j_9
+    };
+    Button numberBtn[] = new Button[9];
+    Integer[] numberID={
+            R.id.one, R.id.two, R.id.three, R.id.four, R.id.five,
+            R.id.six, R.id.seven, R.id.eight, R.id.nine
+    };
+
+    Button Reset,Answer,Cancel;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        Reset = findViewById(R.id.reset);
+        Answer = findViewById(R.id.answer);
+        Cancel = findViewById(R.id.cancel);
+
+        for(int i=0;i<81;i++){
+            button[i]=(Button)findViewById(btnID[i]);
+        }
+
+        for(int i=0;i<9;i++){
+            System.out.println(i);
+            numberBtn[i]=(Button)findViewById(numberID[i]);
+        }
+
+        for(int i=0; i<button.length; i++){
+            final int INDEX;
+            INDEX = i;
+
+            button[INDEX].setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    if(flag==1){
+                        Toast.makeText(getApplicationContext(), "리셋 후 사용해 주세요.", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                    if(before!=100){
+                        button[before].setBackgroundColor(Color.WHITE);
+                    }
+                    before=INDEX;
+                    button[INDEX].setBackgroundColor(Color.BLACK);
+
+                    /*
+                    touchX=INDEX%9;
+                    touchY=((8-touchX)+INDEX)/10;
+
+                    String X = Integer.toString(touchX);
+                    String Y = Integer.toString(touchY);
+                    Toast.makeText(getApplicationContext(), X+" "+Y, Toast.LENGTH_LONG).show();
+                    */
+
+                }
+            });
+        }
+
+        for(int i=0; i<numberBtn.length; i++){
+            final int INDEX;
+            INDEX = i;
+
+            numberBtn[INDEX].setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) {
+                    if(flag==1){
+                        Toast.makeText(getApplicationContext(), "리셋 후 사용해 주세요.", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                    if(before!=100){
+                        button[before].setBackgroundColor(Color.WHITE);
+                        button[before].setText(INDEX+1+"");
+
+                        touchX=before%9;
+                        touchY=((8-touchX)+before)/10;
+                        grid[touchX][touchY]=INDEX+1;
+                        System.out.println(touchX+" "+touchY);
+                        System.out.println(grid[touchX][touchY]);
+
+                    }
+                }
+            });
+        }
+
+        Reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a,b;
+                before=100;
+                for(int i=0;i<81;i++){
+                    button[i].setBackgroundColor(Color.WHITE);
+                    button[i].setText("");
+                }
+
+                flag=0;
+
+                for (a = 0; a < 9; a++)
+                {
+                    for (b = 0; b < 9; b++)
+                    {
+                        grid[a][b]=0;
+                        System.out.print( grid[b][a]);
+                    }
+                    System.out.println();
+                }
+            }
+        });
+
+        Answer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a,b;
+                if(flag==1){
+                    Toast.makeText(getApplicationContext(), "리셋 후 사용해 주세요.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(check(grid)){
+                    solve(grid);
+                    for(int i=0;i<81;i++)
+                    {
+                        button[i].setBackgroundColor(Color.WHITE);
+                        a=i%9;
+                        b=((8-a)+i)/10;
+                        button[i].setText(grid[a][b]+"");
+                    }
+
+                }
+
+            }
+        });
+
+        Cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(flag==1){
+                    Toast.makeText(getApplicationContext(), "리셋 후 사용해 주세요.", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(before!=100){
+                    button[before].setBackgroundColor(Color.WHITE);
+                    button[before].setText("");
+
+                    touchX=before%9;
+                    touchY=((8-touchX)+before)/10;
+                    grid[touchX][touchY]=0;
+                    System.out.println(touchX+" "+touchY);
+                    System.out.println(grid[touchX][touchY]);
+
+                }
+            }
+        });
+    }
+    public void solve(int grid[][]){
+        int x, y, n;
+        for (y = 0; y < 9; y++)
+        {
+            for (x = 0; x < 9; x++)
+            {
+                if (grid[y][x] == 0)
+                {
+                    for (n = 1; n < 10; n++)
+                    {
+                        if (possible(y, x, n, grid))
+                        {
+                            grid[y][x] = n;
+                            solve(grid);
+                            if (flag==1)
+                            {
+                                return;
+                            }
+
+                            grid[y][x] = 0;
+                        }
+                    }
+
+                    return;
+                }
+            }
+        }
+
+        int a, b;
+        for (a = 0; a < 9; a++)
+        {
+            for (b = 0; b < 9; b++)
+            {
+                System.out.print( grid[a][b]);
+            }
+            System.out.println();
+        }
+        flag = 1;
+    }
+
+    public boolean possible(int y, int x,int n,int grid[][])
+    {
+        int i, j, x0, y0;
+        for (i = 0; i < 9; i++)
+        {
+            if(x==i && checkInt==1){
+                i++;
+                if(i==9)
+                    break;
+            }
+            if (grid[y][i] == n){
+
+                return false;
+            }
+
+        }
+        for (i = 0; i < 9; i++)
+        {
+            if(y==i && checkInt==1){
+                i++;
+                if(i==9)
+                    break;
+            }
+            if (grid[i][x] == n){
+
+                return false;
+            }
+        }
+
+        x0 = (x / 3) * 3;
+        y0 = (y / 3) * 3;
+
+        for (i = 0; i < 3; i++)
+        {
+            for (j = 0; j < 3; j++)
+            {
+
+                if(y0+i==y && x0+j==x && checkInt==1){
+                    if(i==2){
+                        if(j==2)
+                            break;
+                        j++;
+                    }else{
+                        i++;
+                    }
+                }
+                if (grid[y0 + i][x0 + j] == n){
+
+                    return false;
+                }
+            }
+        }
+        checkInt=0;
+        return true;
+    }
+    public boolean check(int grid[][]){
+        int a,b;
+        for(a=0;a<9;a++)
+        {
+            for(b=0;b<9;b++)
+            {
+                if(grid[a][b]!=0)
+                {
+                    checkInt=1;
+                    if(possible(a,b,grid[a][b],grid))
+                    {
+                        System.out.println("성공");
+
+                    }else{
+                        Toast.makeText(getApplicationContext(), "잘못된 입력이 있습니다.", Toast.LENGTH_LONG).show();
+                        return false;
+                    }
+                }
+
+            }
+        }
+        return true;
+
+    }
+}
